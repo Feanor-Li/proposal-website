42d5a042b6bead54c6e82c16c18f3612ce1a7500
李坪轩 <lipingxuan@lipingxuandeMacBook-Air.local>
2019-10-23 14:52:28 +0800

    First page

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		autoScrolling:true,
+        scrollHorizontally: true,
+        navigation:true,
+        slidesNavigation:true,
+        anchors:["firstpage","secondpage","thirdpage"]
+	});
+});
